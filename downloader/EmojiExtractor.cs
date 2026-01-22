4394cc76742828d2ce0a2705c591db0b1c261d00
FriskIsGit <friskisgit@users.noreply.github.com>
2026-01-22 22:37:28 +0100

    Update extraction

--- downloader/EmojiExtractor.cs
+++ downloader/EmojiExtractor.cs
@@ -1,25 +1,11 @@
-﻿using WebScrapper.scrapper;
+﻿using System.Diagnostics;
+using WebScrapper.scrapper;
 
 namespace emoji_list_downloader.downloader; 
 
 public class EmojiExtractor {
     string EMOJIS_WIKI = "https://emojis.wiki/all-emojis";
-     const string SMILEYS_AND_EMOTION_CATEGORY = "smileys-and-emotion-category";
-    const string PEOPLE_AND_BODY_CATEGORY = "people-and-body-category";
-    const string ANIMALS_AND_NATURE_CATEGORY = "animals-and-nature-category";
-    const string FOOD_AND_DRINK_CATEGORY = "food-and-drink-category";
-    const string TRAVEL_AND_PLACES_CATEGORY = "travel-and-places-category";
-    const string ACTIVITIES_CATEGORY = "activities-category";
-    const string OBJECTS_CATEGORY = "objects-category";
-    const string SYMBOLS_CATEGORY = "symbols-category";
-    const string FLAGS_CATEGORY = "flags-category";
-    const string COMPONENT_CATEGORY = "component-category";
 
-    private static string[] CATEGORIES = {
-        SMILEYS_AND_EMOTION_CATEGORY, PEOPLE_AND_BODY_CATEGORY, ANIMALS_AND_NATURE_CATEGORY, FOOD_AND_DRINK_CATEGORY,
-        TRAVEL_AND_PLACES_CATEGORY, ACTIVITIES_CATEGORY, OBJECTS_CATEGORY, SYMBOLS_CATEGORY, FLAGS_CATEGORY, COMPONENT_CATEGORY
-    };
-    
     private readonly ExtendedHttpClient client = new(new HttpClientHandler { AllowAutoRedirect = true }) {
         Timeout = TimeSpan.FromSeconds(60)
     };
@@ -29,37 +15,26 @@ public class EmojiExtractor {
         SimpleResponse response = client.get(EMOJIS_WIKI);
         HtmlDoc doc = new HtmlDoc(response.content);
 
-        int index = 70000;
-        foreach (string category in CATEGORIES) {
-            Tag? categoryDiv = doc.FindFrom("div", index, Compare.Exact("id", category));
-            if (categoryDiv == null) {
-                Console.WriteLine("Category div " + category + " not found, skipping");
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        
+        List<Tag> emojiAnchors = doc.FindAllFrom("a", 0, 
+            Compare.Exact("class", "w-full"), Compare.KeyAndValuePrefix("href", "/"));
+        foreach (Tag anchor in emojiAnchors) {
+            List<Tag> spans = doc.ExtractTags(anchor, "span");
+            if (spans.Count < 2) {
+                Console.WriteLine("Not enough spans, actual: " + spans.Count);
                 continue;
             }
 
-            List<Tag> emojiAnchors = doc.ExtractTags(categoryDiv, "a");
-            foreach (Tag anchor in emojiAnchors) {
-                if (!anchor.CompareAttributes(
-                        Compare.Exact("class", "w-full"), 
-                        Compare.KeyAndValuePrefix("href", "/"))) {
-                    continue;
-                }
-
-                List<Tag> spans = doc.ExtractTags(anchor, "span");
-                if (spans.Count < 2) {
-                    Console.WriteLine("Not enough spans, actual: " + spans.Count);
-                    continue;
-                }
-
-                string emojiChar = doc.ExtractText(spans[0]);
-                string emojiName = doc.ExtractText(spans[1]);
-                Console.WriteLine(emojiChar + " " + emojiName + " length=" + emojiChar.Length);
+            string emojiChar = doc.ExtractText(spans[0]);
+            string emojiName = doc.ExtractText(spans[1]);
+            Console.WriteLine(emojiChar + " " + emojiName + " length=" + emojiChar.Length);
                 
-                Emoji emoji = new Emoji(emojiChar, emojiName);
-                emojis.Add(emoji);
-            }
-            index = categoryDiv.EstimateEndOffset();
+            Emoji emoji = new Emoji(emojiChar, emojiName);
+            emojis.Add(emoji);
         }
+        Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms elapsed while scraping " + response.content.Length + " chars of HTML");
         
         return emojis;
     }
